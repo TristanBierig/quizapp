a0ddb7a8663547a6802a396996462f38f9846c00
Tristan Bierig <trista.bierig@gmail.com>
2023-04-17 22:06:30 +0200

    json and init function start

---
+++ script.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+        "question": "Wie viele Masten hat ein Schooner?",
+        "image": "./img/question0.jpg",
+        "answer_1": "3",
+        "answer_2": "1",
+        "answer_3": "2",
+        "answer_4": "4",
+        "right_answer": 3
+    },
+    {
+        "question": "Mozart war ein begnadeter Musiker auf welchem Instrument?",
+        "image": "./img/question1.jpg",
+        "answer_1": "Klavier",
+        "answer_2": "Violine",
+        "answer_3": "E-Gitarre",
+        "answer_4": "Triangel",
+        "right_answer": 1
+    },
+    {
+        "question": "Welcher der folgenden Vögel kann nicht fliegen?",
+        "image": "./img/question2.jpg",
+        "answer_1": "Gartenbaumläufer",
+        "answer_2": "Kleiber",
+        "answer_3": "Ortolan",
+        "answer_4": "Struthio",
+        "right_answer": 4
+    },
+    {
+        "question": "In dem Herrn der Ringe gehört Bilbo Beutlin zu welchem Volk?",
+        "image": "./img/question3.jpg",
+        "answer_1": "Orks",
+        "answer_2": "Hobbits",
+        "answer_3": "Zwerge",
+        "answer_4": "Elben",
+        "right_answer": 2
+    },
+    {
+        "question": "In welchem Land befindet sich Europas größte Wanderdüne?",
+        "image": "./img/question4.jpg",
+        "answer_1": "Italien",
+        "answer_2": "Frankreich",
+        "answer_3": "Spanien",
+        "answer_4": "Dänemark",
+        "right_answer": 2
+    }
+];
+
+
+let currentQuestion = 0;
+
+
+function init() {
+    let max = document.getElementById('max-questions');
+    let number = questions.length;
+    showQuestion();
+    max.innerHTML = number;
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question-image').src = question['image'];
+    document.getElementById('question-text').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
